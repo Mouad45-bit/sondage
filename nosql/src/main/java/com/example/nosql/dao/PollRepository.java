f30871ea0666524abfbb3d534c3623eae002cc09
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-02 14:56:56 +0100

    create PollRepository v1 -- dao

---
+++ nosql/src/main/java/com/example/nosql/dao/PollRepository.java
@@ -0,0 +1,20 @@
+package com.example.nosql.dao;
+
+import com.example.nosql.model.Poll;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
+
+public interface PollRepository extends MongoRepository<Poll, String> {
+    Page<Poll> findAll(Pageable pageable);
+    //
+    Page<Poll> findByTitleContainingIgnoreCase(String q, Pageable pageable);
+    //
+    Page<Poll> findByStatus(String status, Pageable pageable);
+    //
+    @Query("{ $and: [ { 'dateStart': { $gte: ?0 } }, { 'dateEnd': { $lte: ?1 } } ] }")
+    Page<Poll> findContained(LocalDateTime dateStart, LocalDateTime dateEnd, Pageable pageable);
+}
