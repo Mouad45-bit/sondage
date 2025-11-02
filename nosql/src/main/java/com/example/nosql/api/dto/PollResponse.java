6d79f852f89f3d20748c07c3fc4e48918fe66ecd
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-02 19:29:37 +0100

    add setters to PollResponse v2 -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/PollResponse.java
+++ nosql/src/main/java/com/example/nosql/api/dto/PollResponse.java
@@ -2,7 +2,7 @@ package com.example.nosql.api.dto;
 
 import jakarta.validation.constraints.*;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class PollResponse {
@@ -10,8 +10,17 @@ public class PollResponse {
     @NotBlank public String title;
     public String description;
     @NotBlank public String status;
-    @NotNull public LocalDate dateStart;
-    @NotNull public LocalDate dateEnd;
+    @NotNull public LocalDateTime dateStart;
+    @NotNull public LocalDateTime dateEnd;
     @Size(min = 2) public List<@NotBlank String> options;
     @NotBlank public String userId;
+    //
+    public void setId(String id) {this.id = id;}
+    public void setTitle(String title) {this.title = title;}
+    public void setDescription(String description) {this.description = description;}
+    public void setStatus(String status) {this.status = status;}
+    public void setDateStart(LocalDateTime dateStart) {this.dateStart = dateStart;}
+    public void setDateEnd(LocalDateTime dateEnd) {this.dateEnd = dateEnd;}
+    public void setOptions(List<@NotBlank String> options) {this.options = options;}
+    public void setUserId(String userId) {this.userId = userId;}
 }
