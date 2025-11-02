955fd80fafa9a2fa17dfc76780de724bc744c9d4
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-02 15:05:29 +0100

    create PollService v1 -- dao

---
+++ nosql/src/main/java/com/example/nosql/service/PollService.java
@@ -0,0 +1,17 @@
+package com.example.nosql.service;
+
+import com.example.nosql.model.Poll;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
+public interface PollService {
+    Page<Poll> listAll(Pageable pageable);
+    //
+    Page<Poll> searchByTitle(String query, Pageable pageable);
+    //
+    Page<Poll> listByStatus(String status, Pageable pageable);
+    //
+    Page<Poll> listContained(LocalDateTime dateStart, LocalDateTime dateEnd, Pageable pageable);
+}
