5a437982ffb80c89e5eab494b5d2b7131474ded6
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-02 20:09:44 +0100

    add missing attributes to PollRequest v1.1 -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
+++ nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
@@ -2,15 +2,17 @@ package com.example.nosql.api.dto;
 
 import jakarta.validation.constraints.*;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class PollRequest {
     @NotBlank @Size(max = 120) public String title;
     @Size(max = 500) public String description;
-    @NotNull public LocalDate dateStart;
-    @NotNull public LocalDate dateEnd;
+    @NotNull public LocalDateTime dateStart;
+    @NotNull public LocalDateTime dateEnd;
+    @NotBlank public String status;
     @Size(min = 2) public List<@NotBlank String> options;
+    @NotBlank public String authorId;
     //
     @AssertTrue(message="dateStart must be before dateEnd")
     public boolean isStartBeforeEnd() {
