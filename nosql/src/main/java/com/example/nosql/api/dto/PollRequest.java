e18f0977feafe29b12ac74c9b1d767c13201a582
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-02 20:12:57 +0100

    switch attributes access. to private in PollRequest v1.2 -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
+++ nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
@@ -6,13 +6,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class PollRequest {
-    @NotBlank @Size(max = 120) public String title;
-    @Size(max = 500) public String description;
-    @NotNull public LocalDateTime dateStart;
-    @NotNull public LocalDateTime dateEnd;
-    @NotBlank public String status;
-    @Size(min = 2) public List<@NotBlank String> options;
-    @NotBlank public String authorId;
+    @NotBlank @Size(max = 120) private String title;
+    @Size(max = 500) private String description;
+    @NotNull private LocalDateTime dateStart;
+    @NotNull private LocalDateTime dateEnd;
+    @NotBlank private String status;
+    @Size(min = 2) private List<@NotBlank String> options;
+    @NotBlank private String authorId;
     //
     @AssertTrue(message="dateStart must be before dateEnd")
     public boolean isStartBeforeEnd() {
