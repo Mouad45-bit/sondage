c90a030aeabeb3668295f1a31b1f4d55eec6d4b1
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-03 20:40:47 +0100

    switch from MongoRepo to @Query -- dao

--- nosql/src/main/java/com/example/nosql/dao/PollRepository.java
+++ nosql/src/main/java/com/example/nosql/dao/PollRepository.java
@@ -3,16 +3,23 @@ package com.example.nosql.dao;
 import com.example.nosql.model.Poll;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.Repository;
+
 
 import java.time.LocalDateTime;
 
-public interface PollRepository extends MongoRepository<Poll, String> {
+public interface PollRepository extends Repository<Poll, String> {
+    //
+    @Query(value = "{}")
+    Page<Poll> findAll(Pageable pageable);
+    //
+    @Query(value = "{ 'title': { $regex: ?0, $options: 'i' } }")
     Page<Poll> findByTitleContainingIgnoreCase(String q, Pageable pageable);
     //
+    @Query(value = "{ 'status': ?0 }")
     Page<Poll> findByStatus(String status, Pageable pageable);
     //
-    @Query("{ $and: [ { 'dateStart': { $lte: ?1 } }, { 'dateEnd': { $gte: ?0 } } ] }")
+    @Query(value = "{ $and: [ { 'dateStart': { $lte: ?1 } }, { 'dateEnd': { $gte: ?0 } } ] }")
     Page<Poll> findOverlapping(LocalDateTime dateStart, LocalDateTime dateEnd, Pageable pageable);
 }
