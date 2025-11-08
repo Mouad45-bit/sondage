de41d755c2b0f0033cbb66ef0ff2a87f74582fc1
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-08 20:49:16 +0100

    delete status in PollRequest -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
+++ nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
@@ -10,7 +10,6 @@ public class PollRequest {
     @Size(max = 500) private String description;
     @NotNull private LocalDateTime dateStart;
     @NotNull private LocalDateTime dateEnd;
-    @NotBlank private String status;
     @Size(min = 2) private List<@NotBlank @Size(max = 80) String> options;
     @NotBlank private String authorId;
     //
@@ -18,4 +17,11 @@ public class PollRequest {
     public boolean isStartBeforeEnd() {
         return dateStart != null && dateEnd != null && dateStart.isBefore(dateEnd);
     }
+    //
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public LocalDateTime getDateStart() { return dateStart; }
+    public LocalDateTime getDateEnd() { return dateEnd; }
+    public List<String> getOptions() { return options; }
+    public String getAuthorId() { return authorId; }
 }
