fc8a03ba34e026574ee232e5b9e6bc7231464297
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-08 23:59:07 +0100

    comment dates in UpdatePollRequest -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/UpdatePollRequest.java
+++ nosql/src/main/java/com/example/nosql/api/dto/UpdatePollRequest.java
@@ -9,12 +9,16 @@ import java.util.List;
 
 public class UpdatePollRequest {
     @Size(max = 500) private String description;
+    /* TODO: dateStart & dateEnd
     @NotNull private LocalDateTime dateStart;
     @NotNull private LocalDateTime dateEnd;
+    */
     @Size(min = 2) private List<@NotBlank @Size(max = 80) String> options;
     //
     public String getDescription() { return description; }
+    /*
     public LocalDateTime getDateStart() { return dateStart; }
     public LocalDateTime getDateEnd() { return dateEnd; }
+     */
     public List<@NotBlank @Size(max = 80) String> getOptions() { return options; }
 }
