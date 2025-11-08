b3f72b4796c2efb344facc5f05d8be3a20fda5c4
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-08 17:45:14 +0100

    adjust some validations in PollRequest v1.3 -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
+++ nosql/src/main/java/com/example/nosql/api/dto/PollRequest.java
@@ -6,12 +6,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class PollRequest {
-    @NotBlank @Size(max = 120) private String title;
+    @NotBlank @Size(min = 3, max = 120) private String title;
     @Size(max = 500) private String description;
     @NotNull private LocalDateTime dateStart;
     @NotNull private LocalDateTime dateEnd;
     @NotBlank private String status;
-    @Size(min = 2) private List<@NotBlank String> options;
+    @Size(min = 2) private List<@NotBlank @Size(max = 80) String> options;
     @NotBlank private String authorId;
     //
     @AssertTrue(message="dateStart must be before dateEnd")
