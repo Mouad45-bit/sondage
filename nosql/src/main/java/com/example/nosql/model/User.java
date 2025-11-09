f1290043d84188c06376c1d198542f6aca7c998d
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-09 13:24:19 +0100

    add passwordHash - User -- model

--- nosql/src/main/java/com/example/nosql/model/User.java
+++ nosql/src/main/java/com/example/nosql/model/User.java
@@ -8,16 +8,20 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id private String id;
     @Indexed(unique = true) private String username;
+    private String passwordHash;
     //
     public User(){};
     //
-    public User(String id, String username) {
+    public User(String id, String username, String passwordHash) {
         this.id = id;
         this.username = username;
+        this.passwordHash = passwordHash;
     }
     //
     public String getId() {return id;}
     public String getUsername() {return username;}
+    public String getPasswordHash() {return passwordHash;}
     //
     public void setUsername(String username) {this.username = username;}
+    public void setPasswordHash(String passwordHash) {this.passwordHash = passwordHash;}
 }
