90737925a1a42709dd27b0119bdab8aec5666287
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-09 14:08:13 +0100

    add @Builder - User -- model

--- nosql/src/main/java/com/example/nosql/model/User.java
+++ nosql/src/main/java/com/example/nosql/model/User.java
@@ -1,10 +1,12 @@
 package com.example.nosql.model;
 
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("users")
+@Builder
 public class User {
     @Id private String id;
     @Indexed(unique = true) private String username;
