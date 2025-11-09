1c8b2d7ecac47c44ab6dd3949fef1e64c71484e5
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-09 16:44:34 +0100

    create JwtService -- security

--- nosql/src/main/java/com/example/nosql/security/JwtService.java
+++ nosql/src/main/java/com/example/nosql/security/JwtService.java
@@ -1,4 +1,30 @@
 package com.example.nosql.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+    private final Key key = Keys.hmacShaKeyFor("change-me".getBytes());
+    //
+    public String generateToken(String username) {
+        long now = System.currentTimeMillis();
+        //
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + 1000L * 60 * 60 * 24))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+    //
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
 }
