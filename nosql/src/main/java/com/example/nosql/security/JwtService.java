bb27096d3c81c4f811c1701fe498439fef4b0dfa
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-09 21:29:14 +0100

    insert jwt.secret into JwtService -- security

--- nosql/src/main/java/com/example/nosql/security/JwtService.java
+++ nosql/src/main/java/com/example/nosql/security/JwtService.java
@@ -2,15 +2,26 @@ package com.example.nosql.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
 public class JwtService {
-    private final Key key = Keys.hmacShaKeyFor("change-me".getBytes());
+    private final Key key;
+    //
+    public JwtService(@Value("${jwt.secret}") String secretBase64) {
+        byte[] decodeBase64 = Decoders.BASE64.decode(secretBase64);
+        if (decodeBase64.length < 32) {
+            throw new IllegalArgumentException("Secret Base64 length must be at least 32 bytes");
+        }
+        this.key = Keys.hmacShaKeyFor(decodeBase64);
+    }
     //
     public String generateToken(String username) {
         long now = System.currentTimeMillis();
