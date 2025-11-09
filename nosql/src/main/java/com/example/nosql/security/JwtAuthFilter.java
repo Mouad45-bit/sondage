2fcfc9c5d258b1c97d7886d38b0842c95ab8f02c
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-09 20:08:53 +0100

    create JwtAuthFilter v2 -- security

--- nosql/src/main/java/com/example/nosql/security/JwtAuthFilter.java
+++ nosql/src/main/java/com/example/nosql/security/JwtAuthFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,5 +24,23 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain chain) throws ServletException, IOException {
         //
+        String header = request.getHeader("Authorization");
+        //
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            //
+            try {
+                String username = jwtService.extractUsername(token);
+                //
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    //
+                    var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    //
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            } catch (Exception ignored) {}
+        }
+        chain.doFilter(request, response);
     }
 }
