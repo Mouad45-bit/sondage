6af9e314bca41beda5d0d5b40d8c096d292fd6fb
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-09 14:30:10 +0100

    create UserService -- service

--- nosql/src/main/java/com/example/nosql/service/UserService.java
+++ nosql/src/main/java/com/example/nosql/service/UserService.java
@@ -1,4 +1,48 @@
 package com.example.nosql.service;
 
-public class UserService {
+import com.example.nosql.api.dto.RegisterRequest;
+import com.example.nosql.dao.UserRepository;
+import com.example.nosql.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository repo;
+    private final PasswordEncoder encoder;
+    private final PasswordEncoder passwordEncoder;
+
+    //
+    public User register(RegisterRequest req) {
+        String username = req.username().toLowerCase().trim();
+        //
+        if (repo.existsByUsername(username)) {
+            throw new IllegalArgumentException("Username is already in use");
+        }
+        //
+        User user = User.builder()
+                .username(username)
+                .passwordHash(passwordEncoder.encode(req.passwordHash()))
+                .build();
+        return repo.save(user);
+    }
+    //
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String usernameLower = username.toLowerCase().trim();
+        //
+        User user = repo.findByUsername(usernameLower).orElseThrow(() ->
+                new UsernameNotFoundException("Username " + username + " not found"));
+        //
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPasswordHash(),
+                null
+        );
+    }
 }
