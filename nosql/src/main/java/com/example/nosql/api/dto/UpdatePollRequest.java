7406a0c7dc617d43b54e8cbfa6b9370348c4b926
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-14 18:48:13 +0100

    bring back dates in UpdatePollRequest -- api.dto

--- nosql/src/main/java/com/example/nosql/api/dto/UpdatePollRequest.java
+++ nosql/src/main/java/com/example/nosql/api/dto/UpdatePollRequest.java
@@ -1,7 +1,6 @@
 package com.example.nosql.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
@@ -9,16 +8,12 @@ import java.util.List;
 
 public class UpdatePollRequest {
     @Size(max = 500) private String description;
-    /* TODO: dateStart & dateEnd
-    @NotNull private LocalDateTime dateStart;
-    @NotNull private LocalDateTime dateEnd;
-    */
+    private LocalDateTime dateStart;
+    private LocalDateTime dateEnd;
     @Size(min = 2) private List<@NotBlank @Size(max = 80) String> options;
     //
     public String getDescription() { return description; }
-    /*
     public LocalDateTime getDateStart() { return dateStart; }
     public LocalDateTime getDateEnd() { return dateEnd; }
-     */
     public List<@NotBlank @Size(max = 80) String> getOptions() { return options; }
 }
