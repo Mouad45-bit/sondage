680eba6af621bea44148a94f5bd2d0a768af3bf5
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-15 20:26:27 +0100

    create UserMapper -- api.mapper

--- nosql/src/main/java/com/example/nosql/api/mapper/UserMapper.java
+++ nosql/src/main/java/com/example/nosql/api/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package com.example.nosql.api.mapper;
 
+import com.example.nosql.api.dto.UserResponse;
+import com.example.nosql.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public UserResponse toResponse(User user) {
+        if (user == null) { return null; }
+        //
+        return new UserResponse(
+                user.getId(),
+                user.getUsername()
+        );
+    }
 }
