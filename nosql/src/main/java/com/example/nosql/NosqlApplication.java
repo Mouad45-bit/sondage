34a1f8224cacb332011ba2e89d21c4d2ba7b3749
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-15 20:02:53 +0100

    add @EnableScheduling to Application -- .

--- nosql/src/main/java/com/example/nosql/NosqlApplication.java
+++ nosql/src/main/java/com/example/nosql/NosqlApplication.java
@@ -2,12 +2,12 @@ package com.example.nosql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NosqlApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NosqlApplication.class, args);
 	}
-
 }
