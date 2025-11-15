645768bae81dbcb3d6b2a0bec214ac98f99e32a1
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-15 19:21:43 +0100

    set permissions v2 - SecurityConfig -- config

--- nosql/src/main/java/com/example/nosql/config/SecurityConfig.java
+++ nosql/src/main/java/com/example/nosql/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                         //
                         .requestMatchers("/auth/register", "/auth/login").permitAll()
                         //
-                        .requestMatchers("/api/polls", "/api/polls/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/polls", "/api/polls/**").permitAll()
                         //
                         .requestMatchers(HttpMethod.POST, "/auth/logout").authenticated()
                         //
