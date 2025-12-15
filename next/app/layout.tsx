f1fc671bacbea475c13e6a9ef7613e93e98ab377
mouad fekkak <mouadfekkak2002@gmail.com>
2025-12-15 11:45:05 +0100

    set Stack Sans Text as a font

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -6,6 +6,14 @@ import { AppHeader } from "./components/AppHeader";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Stack+Sans+Text:wght@200..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="bg-zinc-50 text-zinc-900">
         <AppHeader />
         <main className="mx-auto max-w-6xl px-4 py-6">{children}</main>
